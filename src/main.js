4b6f06a853767e77bfb2977806e593c3d9c0cc77
João Gouveia <a13734@ebsaas.com>
2020-06-07 17:19:40 +0100

    update

--- src/main.js
+++ src/main.js
@@ -17,13 +17,13 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 
 const configOptions = {
-  apiKey: "AIzaSyBHf3CIaClZS_i-EpoZVc0nv0SHwgRGrDM",
-  authDomain: "shoppingcart-auth.firebaseapp.com",
-  databaseURL: "https://shoppingcart-auth.firebaseio.com",
-  projectId: "shoppingcart-auth",
-  storageBucket: "shoppingcart-auth.appspot.com",
-  messagingSenderId: "200397967069",
-  appId: "1:200397967069:web:f0b5a8ea36544c2a8bd534"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 firebase.initializeApp(configOptions);
